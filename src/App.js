5a19b6f9c3ddb2b0073a77fad1effb4f53849c0f
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2022-12-31 19:52:32 +0430

    used the hooks for the first time and created the task component with it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from "react"
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+])
 
   return (
     <div className="container">
       <Header title= {'Task Tracker'} />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
