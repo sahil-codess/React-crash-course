2b122fc5002bc176c6117e4814e9745280379454
sahilkhan <sahilkhanahmadzai4@gmail.com>
2022-12-31 12:52:18 +0430

    learned about props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header title={41} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-export const Header = () => {
+import PropTypes from 'prop-types'
+
+
+export const Header = ({title}) => {
   return (
-    <Header>
-        hello
-    </Header>
+    <header>
+        <h1>{title}</h1>
+    </header>
   )
+}
+
+Header.protoTypes = {
+  title: PropTypes.string,
 }
 
 export default Header
