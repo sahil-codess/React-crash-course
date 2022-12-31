572d348743c8edd12bc47e3e6e2517e5c4d0e2a9
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2022-12-31 18:45:59 +0430

    created and modified the button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,15 @@ import Button from './Button'
 
 
 export const Header = ({title}) => {
+
+  const onClick = () => {
+      console.log('click')
+    }
+
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color = 'green' text = 'hello' />
+        <Button color = 'green' text = 'Add' onClick = {onClick} />
     </header>
   )
 }
