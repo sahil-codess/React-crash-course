0e0ec33967c51ed9f8780d083ca00b564225ec2a
sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-01 12:59:43 +0430

    created the function to hold user input in data structure

--- src/App.js
+++ src/App.js
@@ -28,7 +28,11 @@ function App() {
 
 /// Add Task
 const addTask = (task) => {
-  console.log(task)
+  const id = Math.floor(Math.random() * 10000) +1;
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+
+  console.log(id)
 }
 
 /// Delete Task
