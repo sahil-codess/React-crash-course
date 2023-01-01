f8ae5a9b44f544b53d9a8b45dd61846bef5607de
sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-01 13:24:21 +0430

    made some changes to add button UI like changing it to close when the form is shown and making it red

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import AddTasks, { AddTask } from './components/AddTask'
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -49,8 +51,11 @@ const addTask = (task) => {
  
   return (
     <div className="container">
-      <Header title= {'Task Tracker'} />
-      <AddTask  onAdd={addTask}/>
+      <Header title= {'Task Tracker'}
+       onAdd={() => setShowAddTask(!showAddTask)}
+       showAdd={showAddTask}
+       />
+      {showAddTask && <AddTask  onAdd={addTask}/>}
       {tasks.length > 0 ?
       <Tasks tasks={tasks} 
       onDelete= {deleteTask} 
