6560790b39222fc8b00fc2d622256639b221e5c6
sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-01 13:32:05 +0430

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import AddTasks, { AddTask } from './components/AddTask'
+import  { AddTask } from './components/AddTask'
 
 
 function App() {
