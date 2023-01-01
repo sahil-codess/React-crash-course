1afdeda98c4b0e6c306aa370c4fb6c8b147e7d5d
sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-01 10:22:25 +0430

    created the delete button and created the function to delete task form the UI

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,11 @@
-import { FaTimes} from 'react-icons/fa'
+import { IoMdTrash} from 'react-icons/io'
 
-export const Task = ({task}) => {
+export const Task = ({task, onDelete}) => {
   return (
     <div className="task">
         <h3>
-            {task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}}/>
+            {task.text} <IoMdTrash style={{color: 'red', cursor: 'pointer'}}
+             onClick={() => onDelete(task.id)}/>
         </h3>
         <p>{task.day}</p>
     </div>
