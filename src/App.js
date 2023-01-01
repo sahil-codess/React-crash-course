73f95dd3b064e99a6a33aeaa505923702bf31ded
sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-01 11:47:46 +0430

    created the addtask component and used setstate to set hooks for it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTasks, { AddTask } from './components/AddTask'
 
 
 function App() {
@@ -40,6 +41,7 @@ function App() {
   return (
     <div className="container">
       <Header title= {'Task Tracker'} />
+      <AddTask />
       {tasks.length > 0 ?
       <Tasks tasks={tasks} 
       onDelete= {deleteTask} 
